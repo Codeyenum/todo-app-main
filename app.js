5a9a9b6ae76137fc2774398bbd976da7f45b01fc
Abanum Olise <abanum00@gmail.com>
2023-08-01 13:10:18 +0100

    code clean up

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-let form = document.querySelector("form");
-let todoInput = document.querySelector(".input-box");
-let todoList = document.querySelector(".todo-list");
-let checkBox = document.querySelector("input[type='checkbox']")
-let checkBoxes = document.querySelectorAll("input[type='checkbox']")
-let counterContainer = document.querySelector(".counter");
+const form = document.querySelector("form");
+const todoInput = document.querySelector(".input-box");
+const todoList = document.querySelector(".todo-list");
+const checkBoxes = document.querySelectorAll("input[type='checkbox']");
+const counterContainer = document.querySelector(".counter");
+const statusLists = document.querySelectorAll(".todo-status p");
+const todoItems = todoList.children;
 let counter = 0;
-let statusLists = document.querySelectorAll(".todo-status p")
-let todoItems = todoList.children;
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -15,51 +14,55 @@ form.addEventListener("submit", (e) => {
         counterContainer.children[0].firstElementChild.innerHTML = `${counter}`
         createTodoElements(todoInput.value)
         todoInput.value = "";
-        if (counter > 0) {
-            counterContainer.classList.remove("hide");
-        } else {
-            counterContainer.classList.add("hide");
-        }
+        counterContainer.classList.toggle("hide", counter <= 0);
     }
 })
 
 // permanently sets header checkbox to unchecked
-checkBox.addEventListener("click", () => {
-    checkBox.checked = false;
+checkBoxes[0].addEventListener("click", () => {
+    checkBoxes[0].checked = false;
 })
 
 function removeTodo(e) {
-    this.parentElement.remove();
-    if (this.previousElementSibling.classList.includes("completed")) {
-        counterContainer.children[0].firstElementChild.innerHTML = `${counter}`
-        if (counter > 0) {
-            counterContainer.classList.remove("hide");
-        } else {
-            counterContainer.classList.add("hide");
-        }
-    } else {
-        counter--;
-        counterContainer.children[0].firstElementChild.innerHTML = `${counter}`
-        if (counter > 0) {
-            counterContainer.classList.remove("hide");
-        } else {
-            counterContainer.classList.add("hide");
-        }
-    }
+    const taskElement = e.target;
+    const taskContainer = e.target.parentElement
+    const isTaskCompleted = taskElement.previousElementSibling.classList.contains("completed");
+
+    taskContainer.remove();
+    updateCounter(isTaskCompleted);
 }
 
-function addCompletedStyle(e) {
-    if (e.target.checked == true) {
-        this.parentElement.nextElementSibling.classList.add("completed");
+function updateCounter(isTaskCompleted) {
+    const counterDisplay = counterContainer.children[0].firstElementChild;
+
+    if (isTaskCompleted) {
+        counterDisplay.innerHTML = counter;
+    } else {
         counter--;
-        counterContainer.children[0].firstElementChild.innerHTML = `${counter}`
+        counterDisplay.innerHTML = counter;
     }
-    if (e.target.checked == false) {
-        this.parentElement.nextElementSibling.classList.classList.remove("completed");
-        counter++;
-        counterContainer.children[0].firstElementChild.innerHTML = `${counter}`
-    }
+    counterContainer.classList.toggle("hide", counter <= 0);
 }
+
+function toggleTaskCompletion(e) {
+    const taskContainer = e.target.parentElement.nextElementSibling;
+    const isTaskCompleted = e.target.checked;
+
+    if (isTaskCompleted) {
+        taskContainer.classList.add("completed");
+        counter--;
+    } else {
+        taskContainer.classList.remove("completed");
+        counter++;
+    }
+    updateCounterDisplay();
+}
+
+function updateCounterDisplay() {
+    const counterDisplay = counterContainer.children[0].firstElementChild;
+    counterDisplay.textContent = counter;
+}
+
 
 function createTodoElements(todo) {
     let checkImg = document.createElement("img");
@@ -69,7 +72,6 @@ function createTodoElements(todo) {
     let todoItem = document.createElement("div");
     let pTag = document.createElement("p");
     let crossImg = document.createElement("img");
-    // let itemCounter = 0;
 
     checkImg.src = "./images/icon-check.svg";
     spanTag.className = "checkmark";
@@ -87,18 +89,20 @@ function createTodoElements(todo) {
     todoItem.append(labelTag, pTag, crossImg)
     todoList.append(todoItem);
 
-    checkBox.addEventListener("click", addCompletedStyle)
+    checkBox.addEventListener("click", toggleTaskCompletion)
     crossImg.addEventListener("click", removeTodo)
     todoItem.addEventListener('dragstart', dragStart);
 
     let dragIndex = 0;
     let clone = "";
-    // handle the dragstart    
+
+    // Handle the dragstart    
     function dragStart(e) {
         e.dataTransfer.setData('text/plain', e.target.id);
     }
 
-    todoItem.addEventListener('dragenter', dragEnter)
+    // Add event listeners for drag and drop events
+    todoItem.addEventListener('dragenter', dragEnter);
     todoItem.addEventListener('dragover', dragOver);
     todoItem.addEventListener('dragleave', dragLeave);
     todoItem.addEventListener('drop', drop);
@@ -123,86 +127,86 @@ function createTodoElements(todo) {
 
         e.preventDefault();
         clone = e.currentTarget.cloneNode(true);
+
+        // Add event listeners for the cloned element
         clone.addEventListener('dragstart', dragStart);
-        clone.addEventListener('dragenter', dragEnter)
+        clone.addEventListener('dragenter', dragEnter);
         clone.addEventListener('dragover', dragOver);
         clone.addEventListener('dragleave', dragLeave);
         clone.addEventListener('drop', drop);
 
-        clone.children[0].children[0].addEventListener("click", addCompletedStyle);
-        clone.children[2].addEventListener("click", removeTodo)
-        let dragStartId = e.dataTransfer.getData("text");
+        // Add event listeners for checkbox click and cross icon click
+        clone.children[0].children[0].addEventListener("click", toggleTaskCompletion);
+        clone.children[2].addEventListener("click", removeTodo);
+
+        const dragStartId = e.dataTransfer.getData("text");
 
         if (clone.id !== dragStartId) {
-            let nodeList = e.currentTarget.parentElement.children;
+            const nodeList = e.currentTarget.parentElement.children;
+
             for (let i = 0; i < nodeList.length; i++) {
                 if (nodeList[i].id === dragStartId) {
                     dragIndex = i;
+                    break;
                 }
             }
-            e.currentTarget.parentElement.insertBefore(clone, e.currentTarget.parentElement.children[dragIndex]);
+
+            // Reorder the elements based on the drag-and-drop action
+            e.currentTarget.parentElement.insertBefore(clone, nodeList[dragIndex]);
             e.currentTarget.parentElement.insertBefore(document.getElementById(dragStartId), e.currentTarget.nextSibling);
-            e.currentTarget.remove()
+            e.currentTarget.remove();
         }
     }
+
 }
 
 for (let list of statusLists) {
-    // let todoItems = todoList.children
     list.addEventListener("click", () => {
-        for (let list of statusLists) {
-            list.classList.remove("active");
-        }
+        statusLists.forEach((list) => list.classList.remove("active"));
         list.classList.add("active");
-        if (list.className.includes("active-list")) {
-            for (let item of todoItems) {
-                item.classList.remove("hide");
-            }
-            for (let item of todoItems) {
-                if (item.children[1].className.includes("completed")) {
-                    item.classList.add("hide")
+
+        Array.from(todoItems).forEach((item) => item.classList.remove("hide"));
+
+        if (list.classList.contains("filter-active")) {
+            Array.from(todoItems).forEach((item) => {
+                if (item.children[1].classList.contains("completed")) {
+                    item.classList.add("hide");
                 }
-            }
-        } else if (list.className.includes("completed-list")) {
-            for (let item of todoItems) {
-                item.classList.remove("hide");
-            }
-            for (let item of todoItems) {
-                if (!(item.children[1].className.includes("completed"))) {
-                    item.classList.add("hide")
+            });
+        } else if (list.classList.contains("filter-completed")) {
+            Array.from(todoItems).forEach((item) => {
+                if (!item.children[1].classList.contains("completed")) {
+                    item.classList.add("hide");
                 }
-            }
-        } else {
-            for (let item of todoItems) {
-                item.classList.remove("hide");
-            }
+            });
         }
-    })
+    });
 }
 
-let body = document.querySelector("body");
-let modeToggles = document.querySelectorAll(".toggle");
-let lightMode = document.querySelector(".light-mode");
-let darkMode = document.querySelector(".dark-mode");
+const body = document.querySelector("body");
+const modeToggles = document.querySelectorAll(".toggle");
+const lightMode = document.querySelector(".light-mode");
+const darkMode = document.querySelector(".dark-mode");
 
-for (let toggle of modeToggles) {
+for (const toggle of modeToggles) {
     toggle.addEventListener("click", () => {
         body.classList.toggle("dark");
         lightMode.classList.toggle("hide");
         darkMode.classList.toggle("hide");
-    })
+    });
 }
 
-let clearCompleted = document.querySelector(".clear-all");
+const clearCompleted = document.querySelector(".clear-all");
 const completedTodoItems = [];
-clearCompleted.addEventListener("click", () => {
+
+clearCompleted.addEventListener("click", function () {
     for (let item of todoItems) {
-        console.log(item)
-        if (item.children[1].className.includes("completed")) {
-            completedTodoItems.push(item)
+        if (item.children[1].classList.contains("completed")) {
+            completedTodoItems.push(item);
         }
     }
-    for (let completedItem of completedTodoItems) {
+
+    completedTodoItems.forEach(function (completedItem) {
         completedItem.remove();
-    }
-})
+    });
+});
